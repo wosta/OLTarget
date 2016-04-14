775afdb35d80bc06d6462f8c286f7d1be539ef47
weiwang <weiwang1202@126.com>
2016-04-14 10:06:59 +0800

    Initial Commit

---
+++ DJTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJTarget
+//
+//  Created by 魏旺 on 16/4/14.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJTargetTests/DJTargetTests.m
@@ -0,0 +1,39 @@
+//
+//  DJTargetTests.m
+//  DJTargetTests
+//
+//  Created by 魏旺 on 16/4/14.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DJTargetTests : XCTestCase
+
+@end
+
+@implementation DJTargetTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
